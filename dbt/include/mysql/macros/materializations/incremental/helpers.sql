4c2d5d224e47fb158f91771ad0a879ba5e759b84
resoliwan <resoliwan@naver.com>
2022-11-22 12:00:31 +0900

    add supporting multi unique_key at incemental_delete

--- dbt/include/mysql/macros/materializations/incremental/helpers.sql
+++ dbt/include/mysql/macros/materializations/incremental/helpers.sql
@@ -1,14 +1,24 @@
 
 {% macro incremental_delete(tmp_relation, target_relation, unique_key=none, statement_name="pre_main") %}
-    {%- if unique_key is not none -%}
-    delete
-    from {{ target_relation }}
-    where ({{ unique_key }}) in (
-        select ({{ unique_key }})
-        from {{ tmp_relation }}
-    )
-    {%- endif %}
-
+    {% if unique_key is not none %}
+        {% if unique_key is sequence and unique_key is not string %}
+            delete from {{target_relation }}
+            using {{ tmp_relation }}
+            where (
+                {% for key in unique_key %}
+                    {{ tmp_relation }}.{{ key }} = {{ target_relation }}.{{ key }}
+                    {{ "and " if not loop.last }}
+                {% endfor %}
+            );
+        {% else %}
+            delete from {{ target_relation }}
+            where (
+                {{ unique_key }}) in (
+                select ({{ unique_key }})
+                from {{ tmp_relation }}
+            );
+        {% endif %}
+    {% endif %}
 {%- endmacro %}
 
 {% macro incremental_insert(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
